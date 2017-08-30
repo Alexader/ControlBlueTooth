f98edea82f572770f449c26ca4978a7bcdf3bd4e
Alexader <tyxj58@gmail.com>
2017-08-31 00:54:15 +0800

    1. figure out the right way to connect to HC-05. 2. have to find the way to use OutpurStream to send message.

---
+++ app/src/main/java/com/example/luc/controlbluetooth/ConnectBluetooth.java
@@ -0,0 +1,200 @@
+package com.example.luc.controlbluetooth;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created by LUC on 2017-8-23.
+ */
+
+public class ConnectBluetooth extends Activity {
+    ListView listView;
+
+    public static final int NOTICE_VIEW = 1;
+    final private static int MESSAGE_READ = 100;
+    public static final int RECV_VIEW = 0;
+    int i = 0;
+
+    BluetoothAdapter bluetoothAdapter;
+    ArrayList<BluetoothDevice> devices;
+    public static BluetoothSocket btSocket;
+    ArrayList<String> deviceArray;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blue_tooth_connect);
+
+        listView = (ListView)findViewById(R.id.list);
+        deviceArray = new ArrayList<>();
+
+        // 获取BluetoothAdapter
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(ConnectBluetooth.this,"Device does not support Bluetooth",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Toast.makeText(ConnectBluetooth.this, "蓝牙开启成功", Toast.LENGTH_SHORT).show();
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        } else {
+            Toast.makeText(ConnectBluetooth.this, "蓝牙已开启", Toast.LENGTH_SHORT).show();
+        }
+        //通过适配器获取蓝牙设备的名称
+        bluetoothAdapter.startDiscovery();
+        Set<BluetoothDevice> deviceList = bluetoothAdapter.getBondedDevices();
+        for(BluetoothDevice dev:deviceList){
+            deviceArray.add(dev.getName()+'\n'+dev.getAddress());
+        }
+        listView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, deviceArray));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = listView.getItemAtPosition(position).toString();
+                String macAdrress = item.split("\n")[1];
+                BluetoothDevice device = bluetoothAdapter.getRemoteDevice(macAdrress);
+                try {
+                    Method clientMethod = device.getClass()
+                            .getMethod("createRfcommSocket", new Class[]{int.class});
+                    btSocket = (BluetoothSocket) clientMethod.invoke(device, 1);
+                    connect(btSocket);//连接设备
+
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                    Log.e("crash","down");
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                    Log.e("crash","ret");
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                    Log.e("crash","tur");
+                }
+            }
+        });
+
+    }
+
+
+    /**
+     * 连接蓝牙及获取数据
+     */
+    public void connect(final BluetoothSocket btSocket) {
+        try {
+            btSocket.connect();//连接
+            if (btSocket.isConnected()) {
+                Log.e("----connect--- :", "连接成功");
+                Toast.makeText(getApplicationContext(), "蓝牙连接成功", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent();
+                intent.setClass(ConnectBluetooth.this, HomePage.class);
+                startActivity(intent);
+
+            } else {
+                Toast.makeText(getApplicationContext(), "蓝牙连接失败", Toast.LENGTH_SHORT).show();
+                btSocket.close();
+                listView.setVisibility(View.VISIBLE);
+                Log.e("--------- :", "连接关闭");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("crash","frit");
+        }
+
+    }
+
+//    private class ConnetThread extends Thread {
+//        public void run() {
+//            try {
+//                InputStream inputStream = btSocket.getInputStream();
+//                byte[] data = new byte[1024];
+//                int len = 0;
+//                String result = "";
+//
+//                while (len != -1) {
+//                    if (inputStream.available() > 0 == false) {
+//                        continue;
+//                    } else {
+//                        try {
+//                            Thread.sleep(500);//等待0.5秒，让数据接收完整
+//                            len = inputStream.read(data);
+//                            result = URLDecoder.decode(new String(data, "utf-8"));
+////                          Log.e("----result：----- :", ">>>" + result);
+//                            Message msg = new Message();
+//                            msg.what = MESSAGE_READ;
+//                            msg.obj = result;
+//                            handler.sendMessage(msg);
+//
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//                inputStream.close();
+//                Log.e("--------- :", "关闭inputStream");
+//                if (btSocket != null) {
+//                    btSocket.close();
+//                }
+//            } catch (IOException e) {
+//                Log.e("TAG", e.toString());
+//            }
+//        }
+//
+//    }
+//
+//
+//    private Handler handler = new Handler() {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            switch (msg.what) {
+//                case MESSAGE_READ:
+//                    String result = (String) msg.obj;
+//                    String data = result.split("\\r\\n")[0];
+//                    Log.e("----data：----- :", ">>>" + data);
+//                    if (i < 6) {
+//                        Editable text = (Editable) btAllData.getText();
+//                        text.append(data);
+//                        btAllData.setText(text + "\r\n");
+//                        i++;
+//                    } else {
+//                        btAllData.setText(data + "\r\n");
+//                        i = 0;
+//                    }
+//                    break;
+//            }
+//        }
+//    };
+}
--- app/src/main/java/com/example/luc/controlbluetooth/HomePage.java
+++ app/src/main/java/com/example/luc/controlbluetooth/HomePage.java
@@ -21,31 +21,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
 
 import static android.widget.AdapterView.*;
 
 public class HomePage extends AppCompatActivity {
 
-    private ArrayAdapter<String> adapter = null;
+    ArrayAdapter<String> adapter = null;
     private static final String [] Switch ={"On","Off"};
-    public static final int INFO_ID = 23434;
-    private boolean isRunning = true;
 
-    private TextView Temp;
-    private TextView Humid;
-    private Spinner on_or_off;
+    TextView Temp;
+    TextView Humid;
+    Spinner on_or_off;
 
-    private Button openBluetooth;
-    private BluetoothAdapter bluetoothAdapter;
+    Button openBluetooth;
     private BluetoothDevice device;
     private BroadcastReceiver rec;
-    private ConnectThread connectThread;
-    private ConnectedThread connectedThread;
+    Button ensure_temp;
+    Button ensure_humid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,12 +48,6 @@ public class HomePage extends AppCompatActivity {
         Temp = (TextView) findViewById(R.id.temp);
         Humid = (TextView) findViewById(R.id.humidity_show);
 
-        // 获取BluetoothAdapter
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
-            Toast.makeText(HomePage.this,"Device does not support Bluetooth",Toast.LENGTH_SHORT).show();
-            return;
-        }
         //注册监听事件
         Temp = (TextView) findViewById(R.id.temp_table);
         Humid = (TextView) findViewById(R.id.humidity_show);
@@ -83,148 +70,28 @@ public class HomePage extends AppCompatActivity {
             }
         });
 
-        //使用按钮打开蓝牙开关，并选择连接设备
-        openBluetooth = (Button) findViewById(R.id.openBluetooth);
+        //跳转到蓝牙设置
+        openBluetooth = (Button)findViewById(R.id.openBluetooth);
+        try{
         openBluetooth.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!bluetoothAdapter.isEnabled()) {
-                    Toast.makeText(HomePage.this, "蓝牙开启成功", Toast.LENGTH_SHORT).show();
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(enableBtIntent, 1);
-                } else {
-                    Toast.makeText(HomePage.this, "蓝牙已开启", Toast.LENGTH_SHORT).show();
-                }
-                //从找到的设备中连接
-                Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-                if (pairedDevices.size() > 0) {
-                    for (BluetoothDevice dev : pairedDevices) {
-                        device = dev;
-                    }
-                }
-                connectThread =  new ConnectThread(device);
-                connectThread.start();
+                Intent intent = new Intent(HomePage.this, ConnectBluetooth.class);
+                startActivity(intent);
             }
         });
-    }
-
-    @Override
-    protected void onPause(){
-        isRunning = false;
-        super.onPause();
-    }
-    @Override
-    protected void onResume(){
-        isRunning = false;
-        super.onResume();
-    }
-
-    //开启蓝牙需要新的线程
-    private class ConnectThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final BluetoothDevice mmDevice;
-        private final UUID MY_UUID = UUID.fromString(
-                "00001101-0000-1000-8000-00805f9b34fb");
-
-
-        private ConnectThread(BluetoothDevice device) {
-            BluetoothSocket tmp = null;
-            this.mmDevice = device;
-            try {
-                tmp = this.mmDevice.createRfcommSocketToServiceRecord(MY_UUID);
-            } catch (IOException e) {
-                e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        ensure_temp = (Button)findViewById(R.id.button1);
+        ensure_temp.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String send_data = Temp.getText().toString();
+                InputStream tinputStream = BluetoothSocket.getInputStream();
             }
-            this.mmSocket = tmp;
-        }
-        public void run() {
-            bluetoothAdapter.cancelDiscovery();
-            try {
-                this.mmSocket.connect();
-                connectedThread  = new ConnectedThread(this.mmSocket);
-                connectedThread.start();
-            } catch (IOException connectException) {
-                try {
-                    this.mmSocket.close();
-                } catch (IOException closeException) {
-                    closeException.printStackTrace();
-                }
-                return;
-            }
-        }
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    //用于沟通单片机和Android设备
-    private class ConnectedThread extends Thread {
-        private final BluetoothSocket mmSocket;
-        private final InputStream mmInStream;
-        private final OutputStream mmOutStream;
-        public ConnectedThread(BluetoothSocket socket) {
-            mmSocket = socket;
-            InputStream tmpIn = null;
-            OutputStream tmpOut = null;
-            try {
-                tmpIn = socket.getInputStream();
-                tmpOut = socket.getOutputStream();
-            } catch (IOException e) { }
-            mmInStream = tmpIn;
-            mmOutStream = tmpOut;
-        }
-        public void run() {
-            byte[] buffer = new byte[1024];
-            int begin = 0;
-            int bytes = 0;
-            while (true) {
-                try {
-                    bytes += mmInStream.read(buffer, bytes, buffer.length - bytes);
-                    for(int i = begin; i < bytes; i++) {
-                        if(buffer[i] == "#".getBytes()[0]) {
-                            mHandler.obtainMessage(1, begin, i, buffer).sendToTarget();
-                            begin = i + 1;
-                            if(i == bytes - 1) {
-                                bytes = 0;
-                                begin = 0;
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    break;
-                }
-            }
-        }
-        public void write(byte[] bytes) {
-            try {
-                mmOutStream.write(bytes);
-            } catch (IOException e) { }
-        }
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) { }
-        }
-    }
-
-    //将Connectedthread中的数据跨线程传输到UIThread中去，利用handler进行通信
-    Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            byte[] writeBuf = (byte[]) msg.obj;
-            int begin = (int)msg.arg1;
-            int end = (int)msg.arg2;
-            switch(msg.what) {
-                case 1:
-                    String writeMessage = new String(writeBuf);
-                    writeMessage = writeMessage.substring(begin, end);
-                    break;
-            }
-        }
-    };
+        });
 
 }
+}
+
